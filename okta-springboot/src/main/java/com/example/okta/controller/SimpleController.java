5f08325070f67cf29d20f79aa1b5ddcdec5826cc
Sene68 <senefull68@gmail.com>
2023-04-16 17:32:17 +0900

    api service app으로 변경

--- okta-springboot/src/main/java/com/example/okta/controller/SimpleController.java
+++ okta-springboot/src/main/java/com/example/okta/controller/SimpleController.java
@@ -1,29 +1,15 @@
 package com.example.okta.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
 
     @GetMapping("/")
-    public String home(@AuthenticationPrincipal final OidcUser oidcUser) {
-        return "Welcome, " + oidcUser.getName();
-    }
-
-    @RequestMapping("/attributes")
-    @ResponseBody
-    public String attributes(@AuthenticationPrincipal final OidcUser oidcUser) {
-        return oidcUser.getAttributes().toString();
-    }
-
-    @RequestMapping("/authorities")
-    @ResponseBody
-    public String authorities(@AuthenticationPrincipal final OidcUser oidcUser) {
-        return oidcUser.getAuthorities().toString();
+    public ResponseEntity<String> helloWorld() {
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
     }
 }
